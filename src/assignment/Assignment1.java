eca13e5d23fb17b409dc9b7cc28d6dcd6711ff7c
Ramesh <kanxoramesh@gmail.com>
2019-04-23 14:00:21 +0545

    comment added

--- src/assignment/Assignment1.java
+++ src/assignment/Assignment1.java
@@ -3,25 +3,33 @@ package assignment;
 import java.util.Scanner;
 
 public class Assignment1 {
+    
+    /**
+     * retry is used to count the error generation for input validation 
+     * if any user data is invalid in proper format, retry will be increased
+     * 
+     */
+       static int retry=0;
+
 
 	public static void main(String[] args) {
 
-		//Take scanner input from console
-            String form="";
-		Scanner sc= new Scanner(System.in);
-               System.out.println("Enter Total User : ");
-               int num= sc.nextInt();
+	    //Take scanner input from console
+            String printFormat="";
+            Scanner sc= new Scanner(System.in);
+            
+            System.out.println("Enter Total User : ");
+            int totalNum= sc.nextInt();
                
-               if(num > 10 || num < 5)
+            if(totalNum > 10 || totalNum < 5)
                   System.out.println("The number of individuals is between 5-10 ");
-                else
-               {
-                   boolean isError=false;
-               for(int i=0;i<num;i++)
-               {
-                   String pos="";
-                   
-                   if(i==0)
+            else
+            {
+                boolean isError=false;
+                for(int i=0;i<totalNum;i++)
+                {
+                    String pos="";
+                    if(i==0)
                        pos=1+"st ";
                    else if(i==1)
                        pos=2+"nd ";
@@ -29,15 +37,24 @@ public class Assignment1 {
                        pos=3+"rd ";
                    else
                        pos= pos=(i+1)+"th ";
-                   
-                   form=manageInput(sc, pos,isError,form);
+                    printFormat=manageInput(sc, pos,isError,printFormat);
                 }
                 if(!isError)
-               {
-                   System.out.print(form);
-               }
+                {
+                   System.out.print(printFormat);
+                }
                }
               }
+        
+        /**
+         * check user input data and return boolean value. it validate data 
+         * with required format.
+         * 
+         * @param id is user identifier which is range between 1-100 
+         * @param sbp systolic blood pressure
+         * @param dbp diastolic blood pressure
+         * @return boolean value 
+         */
         
 	public static boolean checkValidation(int id, double sbp, double dbp)
 	{
@@ -55,49 +72,59 @@ public class Assignment1 {
 		return true;
 	}
         
-        static int j=0;
-        public static String manageInput(Scanner sc,String pos,boolean isError,String format)
+        
+        
+        /**
+         * this method manage the input data 
+         * Scanner will be used to take data from console 
+         * and return proper format data to print as out in console
+         * 
+         * @param scanner is used to take input from user console
+         * @param currentPos current position of user  in loop
+         * @param isError check whether error occurs or not
+         * @param format format of string used to print in console
+         * @return string with appended value in format
+         */
+        public static String manageInput(Scanner scanner,String currentPos,boolean isError,String format)
         {
+            
             MAPCalculator mapObj= new MAPCalculator();
            
 
-             System.out.println("Enter " +pos +" User Identifier: ");
-		int id=sc.nextInt();
-		//identifier is of range 1-100
-		System.out.println("Enter "+pos +" User SBP in mmHg: ");
-		double sbp=sc.nextDouble();
+            System.out.println("Enter " +currentPos +" User Identifier: ");
+            int id=scanner.nextInt();
+            //identifier is of range 1-100
+            System.out.println("Enter "+currentPos +" User SBP in mmHg: ");
+	    double sbp=scanner.nextDouble();
 
-		System.out.println("Enter "+pos +" User DBP in mmHg: ");
-		double dbp=sc.nextDouble();
+	    System.out.println("Enter "+currentPos +" User DBP in mmHg: ");
+            double dbp=scanner.nextDouble();
                 
                                
-                if(!checkValidation(id,sbp,dbp))
-                {           
-                    if(j<2)
-                    {
-                        j++;
-                        isError=true;
-                        System.out.println("Enter data again for " +pos +" User ");
-                        manageInput(sc,pos,isError,format);
-                    }
-                    else
-                    {
-                        //you have enter incorrect more times 
-                        //exit 
-                        System.out.println("You have entered incorrect data multiple times");
-                        isError=true;
-                        System.exit(22);
-                    }
+            if(!checkValidation(id,sbp,dbp))
+            {
+                if(retry<2)
+                {
+                    retry++;
+                    isError=true;
+                    System.out.println("Enter data again for " +currentPos +" User ");
+                    manageInput(scanner,currentPos,isError,format);
                 }
                 else
                 {
-                    j=0;
-                    double map= mapObj.value(sbp,dbp);
-                    String category=mapObj.category(map);
-                    format+=id+". The "+category+ " MAP "+ String.format("%.2f", map)+"\n";
-		   
+                 System.out.println("You have entered incorrect data multiple times");
+                 isError=true;
+                 System.exit(22);
                 }
-                return format;
+            }
+            else
+            {
+                retry=0;
+                double map= mapObj.value(sbp,dbp);
+                String category=mapObj.category(map);
+                format+=id+". The "+category+ " MAP "+ String.format("%.2f", map)+"\n";
+            }
+            return format;
         }
 
 }
--- src/assignment/MAPCalculator.java
+++ src/assignment/MAPCalculator.java
@@ -2,32 +2,35 @@ package assignment;
 
 public class MAPCalculator {
 	
+        //CONSTRUCTOR
 	public MAPCalculator()
 	{
 	}
-		public double value(double sbp, double dbp) {
-			double MAP = 1.0/3.0 * sbp + 2.0/3.0 * dbp;
-			return MAP;
-		}
-		
-		
-		public String category(double map) {
-		
-			if(map<70)
-			{
-				return "low";
-
-			}
-			else if(map>100)
-			{
-				return "high";
-
-			}
-			else
-			return "normal";
-			
-		}
-	
-
-	
+        
+        /**
+         * calculate Mean Arterial Pressure 
+         * @param sbp systolic blood pressure
+         * @param dbp diastolic blood pressure
+         * @return MAP (Mean Arterial Pressure )
+         */
+        public double value(double sbp, double dbp) {
+            double MAP = 1.0/3.0 * sbp + 2.0/3.0 * dbp;
+            return MAP;
+	}
+        
+        
+        /**
+         * calculate category of blood pressure 
+         * 
+         * @param map Mean Arterial Pressure 
+         * @return category "low", "high","normal"
+         */
+        public String category(double map) {
+            if(map<70)
+                return "low";
+            else if(map>100)
+                return "high";
+            else
+                return "normal";
+        }
 }
